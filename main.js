62d20ca71656fd5be292b6f7856123b2f97ebb05
thisiscyiran <farzad.dianat@gmail.com>
2022-12-27 17:25:48 +0330

    Add finalResult and playAgain function

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-// function getComputerChoice randomly pick value form Array "choices" and return it.
+let playerScore = 0;
+let computerScore = 0;
+
+// function getComputerChoice randomly pick value form Array "choices" and
+// return it.
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
   return choices[Math.floor(Math.random() * 3)];
@@ -9,49 +13,78 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = prompt("Rock, Paper or Scissors? ");
   computerSelection = getComputerChoice();
 
-  wonRockVsScissors = "You Won, Rock beats Scissors";
-  wonPaperVsRock = "You Won, Paper beats Rock";
-  wonScissorsVsPaper = "You Won, Scissors beats Paper";
-
-  lostRockVsScissors = "You Lost,Rock beats Scissors";
-  lostPaperVsRock = "You Lost, Rock beats Paper";
-  lostScissorsVsPaper = "You Lost, Scissors beats Paper";
-
-  draw = "Draw";
-
   if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-    return draw;
+    console.log(`Draw ${playerScore}-${computerScore}`);
   }
 
   if (
     playerSelection.toLowerCase() === "rock" &&
     computerSelection.toLowerCase() === "paper"
   ) {
-    return lostPaperVsRock;
+    computerScore += 1;
+    console.log(`You Lost, Paper beats Rock , ${playerScore}-${computerScore}`);
   } else if (
     playerSelection.toLowerCase() === "rock" &&
     computerSelection.toLowerCase() === "scissors"
   ) {
-    return wonRockVsScissors;
+    playerScore += 1;
+    console.log(
+      `You Won, Rock beats Scissors, ${playerScore}-${computerScore}`
+    );
   } else if (
     playerSelection.toLowerCase() === "paper" &&
     computerSelection.toLowerCase() === "rock"
   ) {
-    return wonPaperVsRock;
+    playerScore += 1;
+    console.log(`You Won, Paper beats Rock, ${playerScore}-${computerScore}`);
   } else if (
     playerSelection.toLowerCase() === "paper" &&
     computerSelection.toLowerCase() === "scissors"
   ) {
-    return lostScissorsVsPaper;
+    computerScore += 1;
+    console.log(
+      `You Lost,Scissors beats Paper,${playerScore}-${computerScore}`
+    );
   } else if (
     playerSelection.toLowerCase() === "scissors" &&
     computerSelection.toLowerCase() === "rock"
   ) {
-    return lostRockVsScissors;
+    computerScore += 1;
+    console.log(`You Lost,Rock beats Scissors,${playerScore}-${computerScore}`);
   } else if (
     playerSelection.toLowerCase() === "scissors" &&
     computerSelection.toLowerCase() === "paper"
   ) {
-    return wonScissorsVsPaper;
+    playerScore += 1;
+    console.log(
+      `You Won, Scissors beats Paper, ${playerScore}-${computerScore}`
+    );
   }
+}
+
+// function game() would iterate the playRound function 5 times
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playRound();
+  }
+
+  return finalResult();
+}
+
+// function finalResult would declare the winner or loser and return message
+// base on that
+function finalResult() {
+  if (playerScore == computerScore) {
+    return "Draw!";
+  } else if (playerScore > computerScore) {
+    return "Winner, Winner chicken dinner!";
+  } else {
+    return "Game Over!";
+  }
+}
+
+// Function playAgain would reset the Score on both sides
+function playAgain() {
+  playerScore = 0;
+  computerScore = 0;
 }
